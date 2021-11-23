57fc8cb1fb69a76d5bf0779ca553283bc393c9bd
Raúl Casamayor <raul.casamayor.navas@alumnos.upm.es>
2021-11-23 18:39:02 +0100

    Initial commit

---
+++ app/src/main/java/universidad/pmd/pmd_grupo14/MainActivity.java
@@ -0,0 +1,14 @@
+package universidad.pmd.pmd_grupo14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
