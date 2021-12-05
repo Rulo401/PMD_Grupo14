4f30fb1287491b1e84d02593b38fa4b99f3deaa0
Rafael Alonso Sirera <rafael.alonso.sirera@alumnos.upm.es>
2021-12-05 20:17:12 +0100

    Avance en edición de noticias 2

--- app/src/main/java/upm/pmd/grupo14/models/article/ArticleAdapter.java
+++ app/src/main/java/upm/pmd/grupo14/models/article/ArticleAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,8 +15,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 import upm.pmd.grupo14.ArticleDetailActivity;
+import upm.pmd.grupo14.ArticleEditActivity;
 import upm.pmd.grupo14.MainActivity;
 import upm.pmd.grupo14.R;
+import upm.pmd.grupo14.models.appContext.LogContext;
 
 public class ArticleAdapter extends BaseAdapter {
 
@@ -51,6 +54,31 @@ public class ArticleAdapter extends BaseAdapter {
         ((TextView)view.findViewById(R.id.txt_title)).setText(articles.get(i).getTitle());
         ((TextView)view.findViewById(R.id.txt_abstract)).setText(articles.get(i).getResume());
         ((ImageView)view.findViewById(R.id.img_thumbnail)).setImageBitmap(articles.get(i).getThubnail().getImg());
+        if(((LogContext)viewGroup.getContext().getApplicationContext()).getLoginToken()!=null){
+            view.findViewById(R.id.lay_loggedActions).setVisibility(View.VISIBLE);
+            Button btn_confirmation = view.findViewById(R.id.btn_confirmation);
+            btn_confirmation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //TODO
+                }
+            });
+            Button btn_edit = view.findViewById(R.id.btn_edit);
+            btn_edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent in = new Intent(viewGroup.getContext(), ArticleEditActivity.class);
+                    in.putExtra(MainActivity.ID_ARTICLE,articles.get(i).getId());
+                }
+            });
+            Button btn_delete = view.findViewById(R.id.btn_delete);
+            btn_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    view.findViewById(R.id.btn_confirmation).setVisibility(View.VISIBLE);
+                }
+            });
+        }
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
